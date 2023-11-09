fde174be7e1e167593c0cf9da7bba023c52472c9
AlehZhuk <burtonflyby@gmail.com>
2023-11-09 10:00:48 +0100

    Code refactoring

--- force-app/main/default/lwc/messageContainer/messageContainer.js
+++ force-app/main/default/lwc/messageContainer/messageContainer.js
@@ -5,15 +5,19 @@
 import {api, LightningElement, wire} from 'lwc';
 import { publish, MessageContext } from "lightning/messageService";
 import MESSAGE_EDIT_CHANNEL from '@salesforce/messageChannel/Message_Edit__c';
+import EDITED_STATUS from '@salesforce/label/c.Edited_Status';
+import Id from '@salesforce/user/Id';
 
 export default class MessageContainer extends LightningElement {
     @api
     message;
     @wire(MessageContext)
     messageContext;
+    currentUserId = Id;
+    editedStatus = EDITED_STATUS;
 
     handleClick() {
-        if (this.message.isOwner) {
+        if (this.isOwner) {
             this.publishEditEvent();
         }
     }
@@ -25,5 +29,19 @@ export default class MessageContainer extends LightningElement {
             tempId: this.message.tempId
         }
         publish(this.messageContext, MESSAGE_EDIT_CHANNEL, payload);
+    }
+
+    get isEdit() {
+        return this.message.status === 'Edit';
+    }
+
+    get isOwner() {
+        return this.currentUserId === this.message.owner;
+    }
+
+    get messageContainerClass() {
+        return this.isOwner ?
+            'cm-message_container slds-text-align_right' :
+            'cm-message_container slds-text-align_left';
     }
 }
--- force-app/main/default/lwc/messagesView/messagesView.js
+++ force-app/main/default/lwc/messagesView/messagesView.js
@@ -5,7 +5,6 @@
 import {LightningElement, api, track, wire} from 'lwc';
 import {subscribe}  from 'lightning/empApi';
 import getMessageHistory from '@salesforce/apex/MessageController.getMessageHistory';
-import Id from '@salesforce/user/Id';
 
 export default class MessagesView extends LightningElement {
 
@@ -16,7 +15,6 @@ export default class MessagesView extends LightningElement {
     messages = [];
     @track
     history = [];
-    currentUserId = Id;
     error;
 
     connectedCallback() {
@@ -57,7 +55,6 @@ export default class MessagesView extends LightningElement {
     }
 
     eventToMessage(message) {
-        let isOwner = this.isOwner(message.CreatedById);
         return {
             id: message.Chat_Message_Id__c,
             tempId: message.Temp_Id__c,
@@ -65,34 +62,18 @@ export default class MessagesView extends LightningElement {
             status: message.Action__c,
             owner: message.CreatedById,
             lastName: message.User_Name__c,
-            date: message.CreatedDate,
-            isOwner: isOwner,
-            align: this.getAlign(isOwner)
+            date: message.CreatedDate
         };
     }
 
     chatMessageToMessage(message) {
-        let isOwner = this.isOwner(message.OwnerId);
         return {
             id: message.Id,
             tempId: message.Temp_Id__c,
             text: message.Message_Text__c,
             status: message.Status__c,
             owner: message.OwnerId,
-            date: message.Created_Date__c,
-            isOwner: isOwner,
-            align: this.getAlign(isOwner)
+            date: message.Created_Date__c
         }
-    }
-
-    isOwner(ownerId) {
-        return ownerId === this.currentUserId;
-    }
-
-    getAlign(isOwner) {
-        if (isOwner)
-            return 'slds-text-align_right';
-        else
-            return 'slds-text-align_left';
     }
 }
